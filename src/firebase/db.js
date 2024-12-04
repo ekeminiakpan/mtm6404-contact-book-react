1b99732743c358c65078318fa2f1bf2b89ddafaf
Ekemini Akpan <akpaooo18@algonquinlive.com>
2024-12-04 16:36:06 -0500

    Complete this assignment

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyDceCt9Eu6ome9mDEgLdEn_23YMc_mB5uY",
-  authDomain: "ekemini-akpan.firebaseapp.com",
-  projectId: "ekemini-akpan",
-  storageBucket: "ekemini-akpan.firebasestorage.app",
-  messagingSenderId: "476871427431",
-  appId: "1:476871427431:web:9528055b2d956d527b9bc2",
-  measurementId: "G-RT9P18CH1E"
+  apiKey: "AIzaSyBIRtmaiSEgionLKhXxqHAcYzEi5_WxJPk",
+  authDomain: "ekemini-project.firebaseapp.com",
+  projectId: "ekemini-project",
+  storageBucket: "ekemini-project.firebasestorage.app",
+  messagingSenderId: "1012264974662",
+  appId: "1:1012264974662:web:be05db6a31490527e68d47"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app); 
